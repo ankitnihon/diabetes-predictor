e1cceeabb388f27ac9a87e9dc6925627de2bdec1
Ankit Narang <45746686+ankitnihon@users.noreply.github.com>
2020-09-12 18:46:12 +0530

    Add files via upload

---
+++ Diabetes Predictor - Deployment.py
@@ -0,0 +1,38 @@
+#Import necessary libraries
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+# loading the data set
+df = pd.read_csv('kaggle_diabetes.csv')
+
+
+#Renaming DiabetesPedigreeFunction to DPF
+df = df.rename(columns = {'DiabetesPedigreeFunction':'DPF'})
+
+# Replacing 0 values to NaN
+df_copy = df.copy(deep=True)
+df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0, np.NaN)
+
+# Replacing NaN value by mean, median depending upon distribution
+df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
+df_copy['BloodPressure'].fillna(df_copy['BloodPressure'].mean(), inplace=True)
+df_copy['SkinThickness'].fillna(df_copy['SkinThickness'].median(), inplace=True)
+df_copy['Insulin'].fillna(df_copy['Insulin'].median(), inplace=True)
+df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
+
+# Data splitting
+X = df_copy.drop(columns='Outcome')
+y = df_copy['Outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 0)
+
+#Model building
+classifier = RandomForestClassifier(n_estimators=20)
+classifier.fit(X_train, y_train)
+
+# Creating a pickle file for the classifier
+filename = 'diabetes-prediction-rfc-model.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# diabetes-predictor
+# Diabetes Prediction[糖尿病予測ツール] - Deployment
+![Kaggle](https://img.shields.io/badge/Dataset-Kaggle-blue.svg) ![Python 3.6](https://img.shields.io/badge/Python-3.6-brightgreen.svg) ![scikit-learnn](https://img.shields.io/badge/Library-Scikit_Learn-orange.svg)
+
+• This repository consists of files required to deploy a ___Machine Learning Web App___ created with ___Flask___ on ___Heroku___ platform.
+
+• Please do like the repository, if it helped you in anyway.
+
+• A glimpse of the web app:
+
+![GIF](readme_resources/diabetes-predictor-web-app.gif)
+
+•　クラウド環境上デプロイした機械学習ウエブアプリです。
+•　データセットはKaggle(カグル)からダウンロードしたものです。
+•　ウエブアプリは日本語版のみになります。
+
